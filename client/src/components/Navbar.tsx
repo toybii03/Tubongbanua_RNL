d9ba5180337ba04a6ada44333d500081891e4935
toybii03 <johnbenjie008@gmail.com>
2025-04-24 00:59:34 +0800

    Update Navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -58,7 +58,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            J RnL
+            John RnL
           </a>
           <button
             className="navbar-toggler"
